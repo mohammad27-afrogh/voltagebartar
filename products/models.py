9d5130081338f274a2b38ece5b8dc1c93db53d50
Mohammad Afroogh <m.afroogh25@gmail.com>
2025-10-18 22:28:59 +0330

    Product information model

--- products/models.py
+++ products/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+    PRODUCT_TYPE = [
+        ('FL', 'Flower'),
+        ('FER', 'Fertilizer'),
+        ('TOOL', 'Gardening Tool'),
+        ('SEED', 'Seed'),
+        ('SOIL', 'Potting Soil'),
+    ]
+
+    name = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True, blank=True)
+    sku = models.CharField(max_length=150, unique=True, blank=True, null=True)
+    product_type = models.CharField(max_length=4, choices=PRODUCT_TYPE)
