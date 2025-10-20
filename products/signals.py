75a7c8430ef6bc7d379ededf53acefbe7d940964
Mohammad Afroogh <m.afroogh25@gmail.com>
2025-10-21 00:02:41 +0330

    add signals file to app products to inventory

---
+++ products/signals.py
@@ -0,0 +1,13 @@
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+
+from .models import Order, Product
+
+@receiver(post_save, sender=Order)
+def update_successful_sales(sender, instance, **kwargs):
+    if instance.status == 'PAI':
+        for item in instance.items.all():
+            product = item.product
+            product.successful_sales_count += int(item.quantity)
+            product.inventory -= item.quantity
+            product.save()
