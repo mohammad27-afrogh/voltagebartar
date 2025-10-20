b5765d904e1e06e42ee539690ed1cbdf52cd21df
Mohammad Afroogh <m.afroogh25@gmail.com>
2025-10-21 00:00:57 +0330

    update class models to products app

--- products/models.py
+++ products/models.py
@@ -1,12 +1,11 @@
 from django.db import models
 from django.utils import timezone
-from django.db.models.signals import post_save
-from django.dispatch import receiver
 from django.contrib.auth import get_user_model
 
 from datetime import date
 from taggit.managers import TaggableManager
 from ckeditor.fields import RichTextField
+from decimal import Decimal, ROUND_HALF_UP
 
 class Product(models.Model):
     PRODUCT_TYPE = [
@@ -30,18 +29,18 @@ class Product(models.Model):
     product_type = models.CharField(max_length=4, choices=PRODUCT_TYPE)
     features = models.ForeignKey('Features', on_delete=models.CASCADE, related_name='product')
     brand = models.ForeignKey('Brand', on_delete=models.SET_NULL, blank=True, null=True, related_name='products', verbose_name='brand')
-    inventory = models.ForeignKey('Inventory', on_delete=models.SET_NULL, related_name='product')
+    inventory = models.ForeignKey('Inventory', on_delete=models.SET_NULL, null=True, related_name='product')
     commodity_status = models.CharField(max_length=3, choices=INVENTORY_STATUS, default='AVA')
     successful_sales_count = models.PositiveIntegerField(default=0)
     base_price = models.DecimalField(max_digits=10, decimal_places=2)
     short_description = models.CharField(max_length=100)
-    description = models.RichTextField()
+    description = RichTextField()
     tags = TaggableManager(blank=True)
     date_time_create = models.DateTimeField(auto_now_add=True)
     date_time_modified = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.name
+        return f'{self.name}'
 
     @property
     def is_on_sale(self):
@@ -63,31 +62,31 @@ class Product(models.Model):
 
     @property
     def final_price(self):
-        best_discount = self.active_discounts.order_by('_discount_percentage').first()
+        best_discount = self.active_discounts.order_by('discount_percentage').first()
 
         if best_discount:
             discount_factor = 1 - (best_discount.discount_percentage / 100)
             discounted_price = self.base_price * discount_factor
-            return discounted_price.quantize(models.DecimalField().quantize(0))
+            return discounted_price.quantize(Decimal('0.01'), rounding=ROUND_HALF_UP)
         else:
-            return self.base_price
+            return f'{self.base_price}'
 
 class Discount(models.Model):
     product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='discounts')
     discount_percentage = models.DecimalField(max_digits=5, decimal_places=2)
     start_date = models.DateField(default=date.today)
-    end_date = models.DateField(default=True)
+    end_date = models.DateField(default=date.today)
     is_active = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.product
+        return f'{self.product.name} - {self.discount_percentage}'
 
 class Category(models.Model):
     name = models.CharField(max_length=100, unique=True)
     slug = models.SlugField(unique=True)
-    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, nul=True, blank=True, related_name='subcategories')
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategories')
 
-    def __Str__(self):
+    def __str__(self):
         return self.name
 
 class Inventory(models.Model):
@@ -97,7 +96,7 @@ class Inventory(models.Model):
         ('SPE', 'Special'),
     ]
     status = models.CharField(max_length=3, choices=INVENTORY_STATUS, default='AVA')
-    inventory = models.DecimalField(max_digits=4, decimal_places=2, default=0)
+    inventory = models.PositiveIntegerField(default=0)
 
     def __str__(self):
         return self.status
@@ -118,20 +117,20 @@ class Features(models.Model):
         ('Cm', 'SantiMeter'),
         ('M', 'Meter'),
     ]
-    Length = models.BooleanField(max_length=3, choices=PRODUCT_NUMBER_UNIT, blank=True, null=True)
-    Width = models.BooleanField(max_length=3, choices=PRODUCT_NUMBER_UNIT, blank=True, null=True)
-    Height = models.BooleanField(max_length=3, choices=PRODUCT_NUMBER_UNIT, blank=True, null=True)
-    pot_size = models.BooleanField(max_length=5, blank=True, null=True)
+    Length = models.DecimalField(max_length=3, choices=PRODUCT_NUMBER_UNIT, blank=True, null=True)
+    Width = models.DecimalField(max_length=3, choices=PRODUCT_NUMBER_UNIT, blank=True, null=True)
+    Height = models.DecimalField(max_length=3, choices=PRODUCT_NUMBER_UNIT, blank=True, null=True)
+    pot_size = models.CharField(max_length=5, blank=True, null=True)
 
     unit = models.CharField(max_length=4, choices=PRODUCT_SOLIDS_UNIT, blank=True, null=True)
     weight = models.CharField(max_length=2, choices=PRODUCT_LIQUID_UNIT, blank=True, null=True)
 
     ingredients = models.CharField(max_length=250)
-    care_tips = models.RichTextField(blank=True, null=True, verbose_name='care tipe')
-    usage_instructions = models.RichTextField(blank=True, null=True, verbose_name='usage instructions')
+    care_tips = RichTextField(blank=True, null=True, verbose_name='care tipe')
+    usage_instructions = RichTextField(blank=True, null=True, verbose_name='usage instructions')
 
     def __str__(self):
-        return self.unit
+        return f'Features for product unit: {self.unit or '-'}'
 
 class Order(models.Model):
     PAYMENT_STATUS = [
@@ -152,23 +151,14 @@ class OrderItem(models.Model):
     quantity = models.DecimalField(max_digits=5, decimal_places=2)
 
     def __str__(self):
-        return self.order
-
-    @receiver(post_save, sender=Order)
-    def update_successful_sales(sender, instance, **kwargs):
-        if instance.status == 'PAID':
-            for item in instance.items.all():
-                product = item.product
-                product.successful_sales_count += int(item.quantity)
-                product.inventory -= item.quantity
-                product.save()
+        return f'{self.quantity} * {self.product.name}'
 
 class Brand(models.Model):
     name = models.CharField(max_length=100, unique=True, verbose_name='Brand name')
-    description = models.RichTextField(blank=True, null=True, verbose_name='description')
+    description = RichTextField(blank=True, null=True, verbose_name='description')
 
     def __str__(self):
-        return self.name
+        return f'{self.name}'
 
     class Meta:
         verbose_name = 'brand'
@@ -180,8 +170,8 @@ class Comment(models.Model):
     user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
     time_release_comment = models.DateTimeField(auto_now_add=True)
     update_to = models.DateTimeField(auto_now=True)
-    body_comment = models.RichTextField()
-    answer_comment = models.ForeignKey(self, null=True, blank=True)
+    body_comment = RichTextField()
+    answer_comment = models.ForeignKey('self', null=True, blank=True)
 
     def __str__(self):
         return self.product
