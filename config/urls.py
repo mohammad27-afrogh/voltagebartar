9f971801c08889200ea016b4ddec76a3000515c4
Mohammad Afroogh <m.afroogh25@gmail.com>
2025-10-23 18:14:38 +0330

    add url rosetta and translation to products app

--- config/urls.py
+++ config/urls.py
@@ -21,5 +21,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('ratings/', include('star_ratings.urls', namespace='ratings')),
+    path('rosetta/', include('rosetta.urls')),
     path('', include('pages.urls')),
 ]
