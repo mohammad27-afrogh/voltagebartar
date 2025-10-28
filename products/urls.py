0815e1097722656dd5b810ab8a83162e8da442f1
Mohammad Afroogh <m.afroogh25@gmail.com>
2025-10-28 10:50:30 +0330

    update detail view and add show comments

--- products/urls.py
+++ products/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path, include
 
-from .views import ProductListView, ProductDetailView
+from .views import ProductListView, product_detail_view
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
-    path('<slug:product_slug>/', ProductDetailView.as_view(), name='product_detail_by_slug'),
+    # path('<slug:product_slug>/', ProductDetailView.as_view(), name='product_detail_by_slug'),
+    path('<slug:product_slug>/', product_detail_view, name='product_detail_by_slug'),
+
 ]
--- products/views.py
+++ products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 
 from .models import  Product
@@ -8,9 +8,17 @@ class ProductListView(generic.ListView):
     template_name = 'products/product_list.html'
     context_object_name = 'products'
 
-class ProductDetailView(generic.DetailView):
-    model = Product
-    template_name = 'products/product_detail.html'
-    context_object_name = 'product'
-    slug_field = 'slug'
-    slug_url_kwarg = 'product_slug'
+# class ProductDetailView(generic.DetailView):
+#     model = Product
+#     template_name = 'products/product_detail.html'
+#     context_object_name = 'product'
+#     slug_field = 'slug'
+#     slug_url_kwarg = 'product_slug'
+
+def product_detail_view(request, product_slug):
+    product_comment = get_object_or_404(Product, slug=product_slug)
+    product_comments = product_comment.comments.all()
+    return render(request, 'products/product_detail.html',{
+        'product': product_comment,
+        'comments': product_comments,
+    })
