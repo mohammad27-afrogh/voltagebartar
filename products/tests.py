11900e71e92c004a3a9f53045221cf52bd61c2a4
Mohammad Afroogh <m.afroogh25@gmail.com>
2025-11-02 21:58:28 +0330

    add test url product detail

--- products/tests.py
+++ products/tests.py
@@ -1,5 +1,9 @@
-from django.test import TestCase
+from django.test import TestCase, Client
 from django.shortcuts import reverse
+from django.utils import timezone
+from decimal import Decimal
+
+from .models import Product, Category, Features
 
 
 class ProductListViewTest(TestCase):
@@ -18,3 +22,52 @@ class ProductListViewTest(TestCase):
     def test_product_list_template_used(self):
         response = self.client.get(reverse('product_list'))
         self.assertTemplateUsed((response, 'product_list.html'))
+
+
+class ProductDetailViewTest(TestCase):
+    def setUp(self):
+
+        self.test_category = Category.objects.create(
+            name = 'Test',
+            slug = 'test',
+        )
+
+        self.test_features = Features.objects.create(
+            name_features = 'Test_Standard',
+            weight = Decimal('500.00'),
+            weight_unit = 'GR',
+        )
+
+        self.test_product = Product.objects.create(
+            name = 'product test 1',
+            slug = 'product_test_1',
+            sku = 'TEST_0012',
+            category = self.test_category,
+            features = self.test_features,
+            base_price = Decimal('15000.00'),
+            product_type = 'FER',
+            commodity_status = 'AVA',
+            short_description = 'a product test',
+            description = 'a product test 1 to the Fertilizer',
+        )
+        self.client = Client()
+
+    def test_product_detail_view_url_success(self):
+        url = reverse('product_detail_by_slug', kwargs={
+            'product_slug': self.test_product.slug
+        })
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 200)
+
+        self.assertContains(response, self.test_product.name)
+        self.assertContains(response, 'a product test')
+
+    def test_product_detail_view_not_found(self):
+        non_existent_slug = 'this_slug_dose_not_exist'
+        url = reverse('product_detail_by_slug', kwargs={
+            'product_slug': non_existent_slug
+        })
+
+        response = self.client.get(url)
+        self.assertEqual(response.status_code, 404)
--- products/urls.py
+++ products/urls.py
@@ -6,5 +6,4 @@ urlpatterns = [
     path('', ProductListView.as_view(), name='product_list'),
     # path('<slug:product_slug>/', ProductDetailView.as_view(), name='product_detail_by_slug'),
     path('<slug:product_slug>/', product_detail_view, name='product_detail_by_slug'),
-
 ]
