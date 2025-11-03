e25de61495432df6dbf436bc03dad5b5d1385c72
Mohammad Afroogh <m.afroogh25@gmail.com>
2025-11-03 19:15:19 +0330

    add class Blog to app blogs and add blog_detail_view address url

--- blogs/models.py
+++ blogs/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
+from django.urls import reverse
 
-# Create your models here.
+from taggit.managers import TaggableManager
+from ckeditor.fields import RichTextField
+
+class Blog(models.Model):
+    name = models.CharField(_('name'), max_length=200)
+    slug = models.SlugField(_('slug'), unique=True, blank=True, null=False)
+    category = models.ForeignKey('Category', on_delete=models.CASCADE, related_name='blogs', verbose_name=_('category'))
+    cover_blog = models.ImageField(_('cover_blog'), upload_to='blog/blog_covers/', blank=True)
+    short_description = models.CharField(_('short_description'), max_length=100)
+    description = RichTextField(_('description'))
+    tags = TaggableManager(_('tags'))
+    date_time_create = models.DateTimeField(_('date_time_create'), default=timezone.now)
+    date_time_modified = models.DateTimeField(_('date_time_modified'), auto_now=True)
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def get_absolute_url(self):
+        return reverse('blog_detail_by_slug', args=[self.slug])
+
+class Category(models.Model):
+    name = models.CharField(_('name'), max_length=100, unique=True)
+    slug = models.SlugField(_('slug'), unique=True)
+    parent_category = models.ForeignKey('self', on_delete=models.CASCADE, null=True, blank=True, related_name='subcategories', verbose_name=_('subcategories'))
+
+    def __str__(self):
+        return f'{self.name}'
+
--- blogs/urls.py
+++ blogs/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path, include
 
-from.views import BlogsHomePageView
+from.views import BlogsHomePageView, blog_detail_view
 
 urlpatterns = [
     path('', BlogsHomePageView.as_view(), name='blogs'),
+    path('<slug:blog_slug>/', blog_detail_view, name='blog_detail_by_slug'),
 ]
