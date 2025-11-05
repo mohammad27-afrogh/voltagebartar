8b52686d20bb85543b6568d1c7373c07a15017a3
Mohammad Afroogh <m.afroogh25@gmail.com>
2025-11-05 19:37:53 +0330

    add TestCartDetailView

--- cart/tests.py
+++ cart/tests.py
@@ -1,3 +1,19 @@
 from django.test import TestCase
+from django.shortcuts import reverse
 
-# Create your tests here.
+class TestCartDetailView(TestCase):
+    def test_cart_detail_view_url(self):
+        response = self.client.get('')
+        self.assertEqual(response.status_code, 200)
+
+    def test_cart_detail_view_url_by_name(self):
+        response = self.client.get(reverse('cart:Cart_detail'))
+        self.assertEqual(response.status_code, 200)
+
+    def test_cart_detail_view_content(self):
+        response = self.client.get(reverse('cart:Cart_detail'))
+        self.assertContains(response, 'cart_detail')
+
+    def testtest_cart_detail_view_template_used(self):
+        response = self.client.get(reverse('cart:Cart_detail'))
+        self.assertTemplateUsed(response, 'cart/cart_detail.html')
