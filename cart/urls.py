052f0af205b732486727c15d39e5c82b44b5c1b7
Mohammad Afroogh <m.afroogh25@gmail.com>
2025-11-05 17:41:32 +0330

    add links urls to app cart

---
+++ cart/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+
+from .views import (
+    cart_detail_view,
+    add_to_cart_view,
+    remove_from_cart,
+    clear_from_cart,
+)
+
+app_name = 'cart'
+
+urlpatterns = [
+    path('', cart_detail_view, name='Cart_detail'),
+    path('add/<int:product_id>/', add_to_cart_view, name='cart_add'),
+    path('remove/<int:product_id>/', remove_from_cart, name='cart_remove'),
+    path('clear/', clear_from_cart, name='cart_clear'),
+]
