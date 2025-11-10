5ebfe8308d543d3a4391cc93d0c41e0454859788
Mohammad Afroogh <m.afroogh25@gmail.com>
2025-11-10 12:07:34 +0330

    add class base views to locations app

--- locations/views.py
+++ locations/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import viewsets, permissions
+from .models import Province, City
+from .serializers import CitySerializer, ProvinceSerializer
 
-# Create your views here.
+
+class ProvinceViewsets(viewsets.ModelViewset):
+    queryset = Province.objects.all()
+    serializer_class = ProvinceSerializer
+    permissions_class = [permissions.IsAuthenticatedOrReadOnly]
+
+class CityViewset(viewsets.ModelViewSet):
+    queryset = City.objects.all()
+    serializer_class = CitySerializer
+    permissions_class = [permissions.IsAuthenticatedOrReadOnly]
+
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        province_id = self.request.query_parms.get('province_id')
+
+        if province_id:
+            return queryset.filter(province_id=province_id)
+        return queryset
