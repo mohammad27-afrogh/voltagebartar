4a6b5ce1eb7509bbe24319eeb3688032a0d077b5
Mohammad Afroogh <m.afroogh25@gmail.com>
2025-11-10 11:42:26 +0330

    add class models Province and City to app locations

--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+
+class Province(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name=_('name_province'))
+
+    class Meta:
+        verbose_name = 'province'
+        verbose_name_plural = 'provinces'
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    province = models.ForeignKey(Province, on_delete=models.CASCADE, related_name = 'relevant_province', verbose_name =_('province'))
+    name = models.CharField(max_length=100, verbose_name =_('city_name'))
+
+    class Meta:
+        verbose_name = 'county'
+        verbose_name_plural = 'cities'
+        unique_together = ('province', 'name')
+
+    def __str__(self):
+        return f'{self.name} ({self.province.name})'
