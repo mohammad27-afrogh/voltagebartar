476945a03cce72bb0ec93ed5e4251656e71a841f
Mohammad Afroogh <m.afroogh25@gmail.com>
2025-11-11 11:45:10 +0330

    add OrderForm to app orders

---
+++ orders/forms.py
@@ -0,0 +1,17 @@
+from django import forms
+from .models import Order
+
+class OrderForm(forms.ModelForm):
+    class Meta:
+        model = Order
+        fields = [
+            'first_name',
+            'last_name',
+            'phone_number',
+            'national_number',
+            'province_address',
+            'city_address',
+            'Exact_address',
+            'email',
+            'order_notes',
+        ]
