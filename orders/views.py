d3d92d8e21aa2f68b5b91de280fdc6bf8a8d8b59
Mohammad Afroogh <m.afroogh25@gmail.com>
2025-11-14 15:01:55 +0330

    add login required and add orderitem to order_create_view

--- orders/views.py
+++ orders/views.py
@@ -1,8 +1,40 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.utils.translation import gettext as _
 
 from .forms import OrderForm
+from cart.cart import Cart
+from .models import OrderItem
 
+@login_required
 def order_create_view(request):
-    return render(request, 'orders/order_create.html', context={
-        'form': OrderForm,
+    cart = Cart(request)
+
+    if len(cart) == 0:
+        messages.warning(request, _('You cannot go to the order page. Please select your product from the store first.'))
+        return redirect('cart:Cart_detail')
+
+    if request.method == 'POST':
+        form = OrderForm(request.POST)
+        if form.is_valid():
+            order_obj = form.save(commit=False)
+            order_obj.user = request.user
+            order_obj.save()
+
+            for item in cart:
+                product = item['product_obj']
+                OrderItem.objects.create(
+                    order = order_obj,
+                    product = product,
+                    quantity = item['quantity'],
+                    price = product.base_price,
+                )
+
+            cart.clear()
+
+    else:
+        form = OrderForm()
+    return render (request, 'orders/order_create.html', context={
+        'form': form,
     })
