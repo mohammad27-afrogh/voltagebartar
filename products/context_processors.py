34fdfdb592ea0bd0716b816b3bfe49c06027e239
Mohammad Afroogh <m.afroogh25@gmail.com>
2025-12-05 22:20:36 +0330

    add section html discounts to file home

--- products/context_processors.py
+++ products/context_processors.py
@@ -1,6 +1,16 @@
-from .models import Category
+from .models import Category, Discount
 
 def context_processors(request):
     parent_categories = Category.objects.filter(parent__isnull=True).prefetch_related('subcategories').order_by('name')
     return {'parent_categories': parent_categories}
 
+def context_processors_discount(request):
+    all_discounts = Discount.objects.filter(is_active=True).select_related('product')
+
+    slider_discounts = list(all_discounts)[:5]
+
+    return {
+        'active_discounts_slider': slider_discounts,
+        'active_discounts_count': len(all_discounts),
+        'active_discounts_full': all_discounts,
+    }
