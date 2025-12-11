f173fe53310305bf4b2dfb253d3667cdc098ddc9
Mohammad Afroogh <m.afroogh25@gmail.com>
2025-12-12 00:51:56 +0330

    update files html and context_processors

--- products/context_processors.py
+++ products/context_processors.py
@@ -4,11 +4,14 @@ from django.utils import timezone
 
 from django.db.models import Q
 
+
 def context_processors(request):
     parent_categories = Category.objects.filter(parent__isnull=True).prefetch_related('subcategories').order_by('name')
     return {'parent_categories': parent_categories}
 
+
 def context_processors_discount(request):
+    # این بخش مخصوص اسلایدر تخفیف‌های فعال است و درست کار می‌کند
     all_discounts = Discount.objects.filter(is_active=True).select_related('product')
 
     slider_discounts = list(all_discounts)[:5]
@@ -19,16 +22,30 @@ def context_processors_discount(request):
         'active_discounts_full': all_discounts,
     }
 
+
 def context_successful_sales(request):
     successful_sales = (Product.objects.filter(successful_sales_count__gte=10)
                         .order_by('-successful_sales_count').prefetch_related('discounts'))
+
+    # *** اصلاح: اضافه کردن ویژگی active_discount ***
+    for product in successful_sales:
+        # این خط، اولین تخفیف فعال را پیدا کرده و به عنوان active_discount اضافه می‌کند
+        product.active_discount = product.discounts.filter(is_active=True).first()
+
     return {'successful_sales': successful_sales}
+
 
 def context_latest_products(request):
     default_time = timezone.now()
     one_month_ago = default_time - timedelta(days=30)
 
     latest_products = (Product.objects.filter(Q(date_time_create__gte=one_month_ago) &
-                                             Q(date_time_create__lte=default_time)))
+                                              Q(date_time_create__lte=default_time))
+                       .prefetch_related('discounts')  # *** اصلاح: اضافه کردن prefetch_related ***
+                       )
 
-    return{'latest_products': latest_products}
+    # *** اصلاح: اضافه کردن ویژگی active_discount ***
+    for product in latest_products:
+        product.active_discount = product.discounts.filter(is_active=True).first()
+
+    return {'latest_products': latest_products}
