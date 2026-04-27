b1018fffb381f160e81c5831648ea814b2b4eb96
Mohammad Afroogh <m.afroogh25@gmail.com>
2026-04-27 13:27:59 +0330

    update icon slug and image

--- blogs/urls.py
+++ blogs/urls.py
@@ -1,8 +1,8 @@
-from django.urls import path, include
+from django.urls import path, re_path, include
 
 from.views import blog_page_view, blog_detail_view
 
 urlpatterns = [
     path('', blog_page_view, name='blogs'),
-    path('<slug:blog_slug>/', blog_detail_view, name='blog_detail_by_slug'),
+    re_path(r'^(?P<blog_slug>[^/]+)/$', blog_detail_view, name='blog_detail_by_slug'),
 ]
--- cart/urls.py
+++ cart/urls.py
@@ -1,4 +1,4 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from .views import (
     cart_detail_view,
@@ -11,7 +11,7 @@ app_name = 'cart'
 
 urlpatterns = [
     path('', cart_detail_view, name='Cart_detail'),
-    path('add/<slug:product_slug>/', add_to_cart_view, name='cart_add'),
-    path('remove/<slug:product_slug>/', remove_from_cart, name='cart_remove'),
+    re_path(r'^(?P<product_slug>[^/]+)/$', add_to_cart_view, name='cart_add'),
+    re_path(r'^(?P<product_slug>[^/]+)/$', remove_from_cart, name='cart_remove'),
     path('clear/', clear_from_cart, name='cart_clear'),
 ]
