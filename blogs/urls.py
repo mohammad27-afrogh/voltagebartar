c0a14861e800bffe698c78e17d33b21fc8a577c7
Mohammad Afroogh <m.afroogh25@gmail.com>
2026-04-29 17:16:44 +0330

    add app_name blog

--- blogs/urls.py
+++ blogs/urls.py
@@ -2,6 +2,8 @@ from django.urls import path, re_path, include
 
 from.views import blog_page_view, blog_detail_view
 
+app_name='blog'
+
 urlpatterns = [
     path('', blog_page_view, name='blogs'),
     re_path(r'^(?P<blog_slug>[^/]+)/$', blog_detail_view, name='blog_detail_by_slug'),
