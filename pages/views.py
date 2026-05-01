cc1401813e68d23012ea8ddf0360dc75c749e26e
Mohammad Afroogh <m.afroogh25@gmail.com>
2026-05-01 03:33:54 +0330

    update files

--- pages/views.py
+++ pages/views.py
@@ -21,37 +21,6 @@ class OrderRegistrationPageView(TemplateView):
     template_name = 'pages/order_registration.html'
 
 def faq_home_page_view(request):
-    # all_faq_items = Questions_and_answers.objects.all()
-
-    # li_LAR = []
-    # li_ORP = []
-    # li_OT = []
-    # li_RG = []
-    # li_DCG = []
-    # li_OS = []
-
-    # for item in all_faq_items:
-    #     if item.category_question == 'LAR':
-    #         li_LAR.append(item)
-    #     elif item.category_question == 'ORP':
-    #         li_ORP.append(item)
-    #     elif item.category_question == 'OT':
-    #         li_OT.append(item)
-    #     elif item.category_question == 'RG':
-    #         li_RG.append(item)
-    #     elif item.category_question == 'DCG':
-    #         li_DCG.append(item)
-    #     elif item.category_question == 'OS':
-    #         li_OS.append(item)
-
-    # context = {
-    #     'li_LAR': li_LAR,
-    #     'li_ORP': li_ORP,
-    #     'li_OT': li_OT,
-    #     'li_RG': li_RG,
-    #     'li_DCG': li_DCG,
-    #     'li_OS': li_OS,
-    # }
     category = request.GET.get('category')
 
     if category:
