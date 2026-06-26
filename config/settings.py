8ff1a26511645eb2829869c64f34d6e319c1c054
Mohammad Afroogh <m.afroogh25@gmail.com>
2026-06-27 01:21:13 +0330

    update file settings.py

--- config/settings.py
+++ config/settings.py
@@ -112,18 +112,18 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-if DEBUG:
-    INSTALLED_APPS += [
-        'debug_toolbar',
-    ]
+# if DEBUG:
+#     INSTALLED_APPS += [
+#         'debug_toolbar',
+#     ]
 
-    MIDDLEWARE += [
-        "debug_toolbar.middleware.DebugToolbarMiddleware",
-    ]
+#     MIDDLEWARE += [
+#         "debug_toolbar.middleware.DebugToolbarMiddleware",
+#     ]
 
-    INTERNAL_IPS = [
-        "127.0.0.1",
-    ]
+#     INTERNAL_IPS = [
+#         "127.0.0.1",
+#     ]
 
 ROOT_URLCONF = 'config.urls'
 
